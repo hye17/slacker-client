349044d8dd58550bb71ee188cc23d3504fa0cccc
Haley <haleyye@hotmail.com>
2019-02-24 22:45:47 +0000

    changed user name

--- scripts/messages.js
+++ scripts/messages.js
@@ -55,8 +55,8 @@ function createArticle( $message ) {
         event.preventDefault( );
         let message = {
          "user": {
-         "name": "Ramses",
-         "image": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
+         "name": "Haley",
+         "image": "http://logo.pizza/img/dog-profile/dog-profile.png"
          },
          "message": {
          "text": event.target.value,
